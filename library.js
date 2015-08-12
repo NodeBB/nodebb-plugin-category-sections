ac640db92219c29025e8c2858080e1843f6ae339
Shogun <shogun@cowtech.it>
2015-08-12 11:47:25 +0200

    Fixed ACP page. - Fixes #1

--- library.js
+++ library.js
@@ -136,7 +136,6 @@ function modifyCategoryTpl(callback) {
 				tpl = tpl.replace(block, '<!-- BEGIN sections --><div class="col-xs-12"><h1>{sections.name}</h1>' + block + '</div><div class="clearfix"></div><!-- END sections -->');
 			}
 
-			tpl = tpl.replace(block, '<!-- BEGIN sections --><div class="col-xs-12 section"><h1>{sections.name}</h1>' + block + '</div><div class="clearfix"></div><!-- END sections -->');
 			tpl = tpl.replace(/\{categories/g, '{sections.categories');
 			tpl = tpl.replace(/IF categories/g, 'IF sections.categories');
 			tpl = tpl.replace(/<!-- IF !disableMasonry -->masonry<!-- ENDIF !disableMasonry -->/, '');
